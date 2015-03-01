1556573726331b6e4ec5e87bb535f9ce22774bdc
amontane <albert.montane@gmail.com>
2015-03-01 22:24:43 +0100

    Fixed mysql escape string

--- dao/mysql.php
+++ dao/mysql.php
@@ -6,7 +6,7 @@
 	function attempt_login ($email, $password) {
 		$link = new mysqli('localhost', $GLOBALS["MORTI_mysql_user"], $GLOBALS["MORTI_mysql_pass"], $GLOBALS["MORTI_mysql_db"]) or die ('Die');
 		// TODO: Error check?
-		$query = "SELECT * FROM user WHERE email = '" . mysql_real_escape_string($email) . "' AND password = MD5('" . mysql_real_escape_string($password) . "')";
+		$query = "SELECT * FROM user WHERE email = '" . mysqli_real_escape_string($link, $email) . "' AND password = MD5('" . mysqli_real_escape_string($link, $password) . "')";
 		$result = mysqli_query($link, $query);
 		
 		if ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
@@ -33,7 +33,7 @@
 	function get_chapter($id) {
 		$link = new mysqli('localhost', $GLOBALS["MORTI_mysql_user"], $GLOBALS["MORTI_mysql_pass"], $GLOBALS["MORTI_mysql_db"]) or die ('Die');
 		// TODO: Error check?
-		$query = "SELECT * FROM chapter where id = " . mysql_real_escape_string($id);
+		$query = "SELECT * FROM chapter where id = " . mysqli_real_escape_string($link, $id);
 		$result = mysqli_query($link, $query);
 		
 		if ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
@@ -46,7 +46,7 @@
 	function get_comments_for_chapter($id) {
 		$link = new mysqli('localhost', $GLOBALS["MORTI_mysql_user"], $GLOBALS["MORTI_mysql_pass"], $GLOBALS["MORTI_mysql_db"]) or die ('Die');
 		// TODO: Error check?
-		$query = "SELECT c.id, u.email, u.username, u.avatar, c.chapter_id, c.text, c.date, c.paragraph FROM comment c, user u where u.email = c.author AND c.chapter_id = " . mysql_real_escape_string($id);
+		$query = "SELECT c.id, u.email, u.username, u.avatar, c.chapter_id, c.text, c.date, c.paragraph FROM comment c, user u where u.email = c.author AND c.chapter_id = " . mysqli_real_escape_string($link, $id);
 		$result = mysqli_query($link, $query);
 		
 		$table = array();
@@ -59,11 +59,11 @@
 	function insert_comment($chapter, $comment, $author) {
 		$link = new mysqli('localhost', $GLOBALS["MORTI_mysql_user"], $GLOBALS["MORTI_mysql_pass"], $GLOBALS["MORTI_mysql_db"]) or die ('Die');
 		// TODO: Error check?
-		$insert = "INSERT INTO comment (author, chapter_id, text, date) VALUES ('". mysql_real_escape_string($author).
-			"', ".mysql_real_escape_string($chapter).", '".$comment."', NOW())";
+		$insert = "INSERT INTO comment (author, chapter_id, text, date) VALUES ('". mysqli_real_escape_string($link, $author).
+			"', ".mysqli_real_escape_string($link, $chapter).", '".$comment."', NOW())";
 
 		if (mysqli_query($link, $insert) === TRUE) {
-			$update = "UPDATE chapter SET last_comment=NOW() WHERE id=" . mysql_real_escape_string($chapter);
+			$update = "UPDATE chapter SET last_comment=NOW() WHERE id=" . mysqli_real_escape_string($link, $chapter);
 			mysqli_query($link, $update);
 			return true;
 		}
@@ -73,11 +73,11 @@
 	function insert_paragraph_comment($chapter, $comment, $author, $paragraph) {
 		$link = new mysqli('localhost', $GLOBALS["MORTI_mysql_user"], $GLOBALS["MORTI_mysql_pass"], $GLOBALS["MORTI_mysql_db"]) or die ('Die');
 		// TODO: Error check?
-		$insert = "INSERT INTO comment (author, chapter_id, text, date, paragraph) VALUES ('". mysql_real_escape_string($author).
-			"', ".mysql_real_escape_string($chapter).", '".$comment."', NOW(), " . $paragraph .")";
+		$insert = "INSERT INTO comment (author, chapter_id, text, date, paragraph) VALUES ('". mysqli_real_escape_string($link, $author).
+			"', ".mysqli_real_escape_string($link, $chapter).", '".$comment."', NOW(), " . $paragraph .")";
 
 		if (mysqli_query($link, $insert) === TRUE) {
-			$update = "UPDATE chapter SET last_comment=NOW() WHERE id=" . mysql_real_escape_string($chapter);
+			$update = "UPDATE chapter SET last_comment=NOW() WHERE id=" . mysqli_real_escape_string($link, $chapter);
 			mysqli_query($link, $update);
 			return true;
 		}
@@ -87,7 +87,7 @@
 	function change_password($new_pass) {
 		$link = new mysqli('localhost', $GLOBALS["MORTI_mysql_user"], $GLOBALS["MORTI_mysql_pass"], $GLOBALS["MORTI_mysql_db"]) or die ('Die');
 		// TODO: Error check?
-		$update = "UPDATE user SET password=MD5('". mysql_real_escape_string($new_pass). "') WHERE email='" . mysql_real_escape_string($_SESSION["MORTI-mail"]) . "'";
+		$update = "UPDATE user SET password=MD5('". mysqli_real_escape_string($link, $new_pass). "') WHERE email='" . mysqli_real_escape_string($link, $_SESSION["MORTI-mail"]) . "'";
 
 		if (mysqli_query($link, $update) === TRUE) {
 			return true;
@@ -98,7 +98,7 @@
 	function change_data($avatar, $uname, $new_pass) {
 		$link = new mysqli('localhost', $GLOBALS["MORTI_mysql_user"], $GLOBALS["MORTI_mysql_pass"], $GLOBALS["MORTI_mysql_db"]) or die ('Die');
 		// TODO: Error check?
-		$update = "UPDATE user SET password=MD5('". mysql_real_escape_string($new_pass). "'),username='".mysql_real_escape_string($uname)."',avatar='".mysql_real_escape_string($avatar)."' WHERE email='" . mysql_real_escape_string($_SESSION["MORTI-mail"]) . "'";
+		$update = "UPDATE user SET password=MD5('". mysqli_real_escape_string($link, $new_pass). "'),username='".mysqli_real_escape_string($link, $uname)."',avatar='".mysqli_real_escape_string($link, $avatar)."' WHERE email='" . mysqli_real_escape_string($link, $_SESSION["MORTI-mail"]) . "'";
 
 		if (mysqli_query($link, $update) === TRUE) {
 			return true;
@@ -110,7 +110,7 @@
 		error_log("hola " . $chapterId . " " . $paragraph);
 		$link = new mysqli('localhost', $GLOBALS["MORTI_mysql_user"], $GLOBALS["MORTI_mysql_pass"], $GLOBALS["MORTI_mysql_db"]) or die ('Die');
 		// TODO: Error check?
-		$update = "UPDATE user SET marker_chapter=". mysql_real_escape_string($chapterId). ", marker_paragraph=".mysql_real_escape_string($paragraph)." WHERE email='" . mysql_real_escape_string($_SESSION["MORTI-mail"]) . "'";
+		$update = "UPDATE user SET marker_chapter=". mysqli_real_escape_string($link, $chapterId). ", marker_paragraph=".mysqli_real_escape_string($link, $paragraph)." WHERE email='" . mysqli_real_escape_string($link, $_SESSION["MORTI-mail"]) . "'";
 		error_log ("&& " . $update . " &&");
 		if (mysqli_query($link, $update) === TRUE) {
 			return true;
@@ -121,7 +121,7 @@
 	function get_marker() {
 		$link = new mysqli('localhost', $GLOBALS["MORTI_mysql_user"], $GLOBALS["MORTI_mysql_pass"], $GLOBALS["MORTI_mysql_db"]) or die ('Die');
 		// TODO: Error check?
-		$query = "SELECT marker_chapter, marker_paragraph FROM user where email = '" . mysql_real_escape_string($_SESSION["MORTI-mail"]) . "' AND marker_chapter IS NOT NULL AND marker_paragraph IS NOT NULL";
+		$query = "SELECT marker_chapter, marker_paragraph FROM user where email = '" . mysqli_real_escape_string($link, $_SESSION["MORTI-mail"]) . "' AND marker_chapter IS NOT NULL AND marker_paragraph IS NOT NULL";
 		$result = mysqli_query($link, $query);
 		
 		if ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
