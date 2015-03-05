c149eb9bfb36ec47f8922830e7b8bf1ef80b0dd7
Myddna <Myddna@users.noreply.github.com>
2015-03-05 10:51:22 +0100

    Update date.php
    Fixing the date format and a double semicolon typo :D

--- dao/date.php
+++ dao/date.php
@@ -1,6 +1,6 @@
 <?php
 	function pretty_date($mysqlTS) {
 		$dateObject = date_create($mysqlTS);
-		return date_format($dateObject, 'd-m-Y H:m');;
+		return date_format($dateObject, 'd-m-Y H:i:s');
 	}
 ?>
